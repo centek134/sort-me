81e0193d12222d20029db7de3a3ccf14cba1690b
centek134 <centek134@wp.pl>
2022-04-17 11:27:38 +0200

    new functionality
    now when we start sorting, compared graph items got diffrent colors
    its done by colorize function
    Functions takes 2 numbers (indexes of arr)
    clearing their styles and later adds them

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,90 +8,53 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var graphId = document.getElementById("graph");
-var arr = Array(50).fill(0).map(function () { return Math.floor(Math.random() * 300); });
-var createGraphItems = function (arr) {
-    arr.forEach(function (item) {
-        var el = document.createElement("div");
-        el.style.height = "".concat(item.toString(), "px");
-        el.style.width = "5px";
-        el.style.backgroundColor = "black";
-        el.style.margin = "0, 5px";
+const graphId = document.getElementById("graph");
+const arr = Array(50).fill(0, 0).map(() => Math.floor(Math.random() * 300));
+const createGraphItems = (arr) => {
+    arr.forEach(item => {
+        const el = document.createElement("div");
+        el.style.height = `${item.toString()}px`;
+        el.classList.add("item");
         graphId.append(el);
     });
 };
-var removeGraphItems = function () {
+const removeGraphItems = () => {
     graphId.innerHTML = "";
 };
-function sleep(ms) {
-    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
-}
-createGraphItems(arr);
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+const colorize = (num_1, num_2) => {
+    const nodes = document.querySelectorAll(".item");
+    nodes.forEach(el => {
+        el.classList.remove("first");
+        el.classList.remove("second");
+    });
+    console.log(num_2);
+    nodes[num_1].classList.add("first");
+    nodes[num_2].classList.add("second");
+};
 function bubbleSort(inputArr) {
-    return __awaiter(this, void 0, void 0, function () {
-        var len, i, j, tmp;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    len = inputArr.length;
-                    i = 0;
-                    _a.label = 1;
-                case 1:
-                    if (!(i < len)) return [3 /*break*/, 6];
-                    j = 0;
-                    _a.label = 2;
-                case 2:
-                    if (!(j < len)) return [3 /*break*/, 5];
-                    return [4 /*yield*/, sleep(5)];
-                case 3:
-                    _a.sent();
-                    if (inputArr[j] > inputArr[j + 1]) {
-                        tmp = inputArr[j];
-                        inputArr[j] = inputArr[j + 1];
-                        inputArr[j + 1] = tmp;
-                        removeGraphItems();
-                        createGraphItems(inputArr);
-                    }
-                    _a.label = 4;
-                case 4:
-                    j++;
-                    return [3 /*break*/, 2];
-                case 5:
-                    i++;
-                    return [3 /*break*/, 1];
-                case 6: return [2 /*return*/, inputArr];
+    return __awaiter(this, void 0, void 0, function* () {
+        let len = inputArr.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = 0; j < len - 1; j++) {
+                colorize(j, j + 1);
+                yield sleep(400);
+                if (inputArr[j] < inputArr[j + 1]) {
+                    let tmp = inputArr[j];
+                    inputArr[j] = inputArr[j + 1];
+                    inputArr[j + 1] = tmp;
+                    removeGraphItems();
+                    createGraphItems(inputArr);
+                }
             }
-        });
+        }
+        return inputArr;
     });
 }
 ;
-var buttonId = document.getElementById("btn").addEventListener("click", function () {
+createGraphItems(arr);
+const buttonId = document.getElementById("btn").addEventListener("click", () => {
     bubbleSort(arr);
 });
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,50 +1,56 @@
 const graphId = document.getElementById("graph");
 
 
-const arr: number[] = Array(50).fill(0).map( () => Math.floor(Math.random() * 300));
+const arr:number[] = Array(50).fill(0,0).map( () => Math.floor(Math.random() * 300));
 
 
 const createGraphItems = (arr:number[]) => {
     arr.forEach( item => {
        const el =  document.createElement("div") as HTMLElement;
         el.style.height = `${item.toString()}px`;
-        el.style.width = "5px";
-        el.style.backgroundColor = "black";
-        el.style.margin = "0, 5px";
+        el.classList.add("item");
         graphId!.append(el);
-    })
-}
-
+    });
+};
 
 const removeGraphItems = () => {
     graphId!.innerHTML = "";
-}
+};
 
-function sleep(ms:number) {
+const sleep = (ms:number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const colorize = (num_1:number,num_2:number) => {
+    const nodes: NodeListOf<HTMLDivElement> = document.querySelectorAll(".item");
+    nodes.forEach(el => {
+        el.classList.remove("first");
+        el.classList.remove("second");
+    });
+    console.log(num_2)
+    nodes[num_1].classList.add("first");
+    nodes[num_2].classList.add("second");
 }
-
-createGraphItems(arr);
-
 async function bubbleSort(inputArr:number[]){
     let len = inputArr.length;
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            await sleep(5);
-            
-            if (inputArr[j] > inputArr[j + 1]) {
+        for (let j = 0; j < len -1 ; j++) {
+            colorize(j,j+1)
+            await sleep(400);
+            if (inputArr[j] < inputArr[j+1]){
                 let tmp = inputArr[j];
-                inputArr[j] = inputArr[j + 1];
-                inputArr[j + 1] = tmp;
+                inputArr[j] = inputArr[j+1];
+                inputArr[j+1] = tmp;
                 removeGraphItems();
-                    createGraphItems(inputArr);
-                }
-
+                createGraphItems(inputArr);
+            }       
         }
     }
     return inputArr;
 };
 
+
+createGraphItems(arr);
 
 const buttonId = document.getElementById("btn")!.addEventListener("click",() => {
     bubbleSort(arr);
